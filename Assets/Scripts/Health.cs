5a8e914073c044d027605f4b973ef258b15dedd4
nguyennamhades <nnhnam2345@gmail.com>
2022-09-18 21:42:58 +0700

    Adding explosion effect

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
@@ -13,7 +14,9 @@ public class Health : MonoBehaviour
         if (damageDealer != null) // nếu không lấy được damageDealer
         {
             TakingDamage(damageDealer.Damage);
+            PlayHitEffect();
             damageDealer.Hit();
+
         }
     }
 
@@ -23,6 +26,15 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void PlayHitEffect () 
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
