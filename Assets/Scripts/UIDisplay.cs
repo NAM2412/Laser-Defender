ab9a1cb45629234ae22311006db1f53ce4460943
nguyennamhades <nnhnam2345@gmail.com>
2022-09-20 21:36:51 +0700

    Display UI by code

---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIDisplay : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] Slider healthSlider;
+    [SerializeField] Health playerHealth;
+
+    [Header("Score")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Awake() 
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    void Start()
+    {
+        healthSlider.maxValue = playerHealth.GetHealth();
+    }
+
+    void Update()
+    {
+        healthSlider.value = playerHealth.GetHealth();
+        scoreText.text = scoreKeeper.Score.ToString("000000000");
+    }
+}
